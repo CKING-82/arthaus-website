e33c22ed93780791226e5e7c4b5569f4a88700b3
callum-king <callum.king@arhtaus.co.uk>
2020-05-22 09:39:46 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var browserSync = require('browser-sync').create();
+
+var useref = require('gulp-useref');
+
+var babel = require('gulp-babel');
+
+var uglify = require('gulp-uglify');
+
+var gulpIf = require('gulp-if');
+
+var cssnano = require('gulp-cssnano');
+
+var imagemin = require('gulp-imagemin');
+
+var cache = require('gulp-cache');
+
+var del = require('del');
+
+var runSequence = require('run-sequence');
+
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+// Reload browser on changes tool
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+
+// Tools to concat files in order & minify css + js
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', babel({
+      presets: ['@babel/env']
+    })))
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+// Remove files from dist folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// Watch for changes to sass, html, and js files > recompile, and reload browser
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
